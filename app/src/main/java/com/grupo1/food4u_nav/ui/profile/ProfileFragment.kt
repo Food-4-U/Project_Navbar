074019ff70a39c6d1338dad434d5e7705702b266
“AutoMendesgit config --global user.email “automendes30@gmail.com <automendes30@gmail.com>
2022-05-12 14:37:54 +0100

    Added ProfileFragment.kt

--- app/src/main/java/com/grupo1/food4u_nav/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/grupo1/food4u_nav/ui/profile/ProfileFragment.kt
@@ -1,16 +1,23 @@
 package com.grupo1.food4u_nav.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.ImageView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.grupo1.food4u_nav.ProductDetailsActivity
+import com.grupo1.food4u_nav.R
 import com.grupo1.food4u_nav.databinding.FragmentHomeBinding
-import com.grupo1.food4u_nav.databinding.ProfileFragmentBinding
+import com.grupo1.food4u_nav.databinding.FragmentProfileBinding
+import projeto.ipca.food4u.grupoI.adapters.HottestAdapter
+import projeto.ipca.food4u.grupoI.models.Item_Menu
 
 class ProfileFragment : Fragment() {
-
-    private var _binding: ProfileFragmentBinding? = null
+    private var _binding: FragmentProfileBinding? = null
 
     private val binding get() = _binding!!
 
@@ -18,16 +25,18 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        _binding = ProfileFragmentBinding.inflate(inflater, container, false)
+    ): View {
+
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+
         return root
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
 }
