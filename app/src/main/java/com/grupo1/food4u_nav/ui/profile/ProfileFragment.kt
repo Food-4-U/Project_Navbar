cf47eecbd740aef64e714dbd42019691ec6bcfc7
DimoN <diogo.vsimoes@gmail.com>
2022-05-12 13:43:56 +0100

    Fixed profile fragment no showing! By Diogo Simões

--- app/src/main/java/com/grupo1/food4u_nav/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/grupo1/food4u_nav/ui/profile/ProfileFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.grupo1.food4u_nav.databinding.FragmentHomeBinding
 import com.grupo1.food4u_nav.databinding.ProfileFragmentBinding
 
 class ProfileFragment : Fragment() {
