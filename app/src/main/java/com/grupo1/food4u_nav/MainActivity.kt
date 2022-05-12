28e9fb4d4a6a8146eea466039d71bd59a785dc87
DimoN <diogo.vsimoes@gmail.com>
2022-05-12 09:48:48 +0100

    Menu changeg name!!!!!! By Diogo Simões

--- app/src/main/java/com/grupo1/food4u_nav/MainActivity.kt
+++ app/src/main/java/com/grupo1/food4u_nav/MainActivity.kt
@@ -49,7 +49,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_search, R.id.navigation_order, R.id.navigation_profile
+                R.id.navigation_home, R.id.navigation_search, R.id.navigation_menu, R.id.navigation_profile
             )
         )
 
