1c9586e42f0d2b07d60d1c67daf234c65767645a
Ozonizador <rnrfnorte@hotmail.com>
2022-05-23 16:33:33 +0100

    Stats test

--- app/src/main/java/com/grupo1/food4u_nav/StatsActivity.kt
+++ app/src/main/java/com/grupo1/food4u_nav/StatsActivity.kt
@@ -5,54 +5,27 @@ import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 import com.grupo1.food4u_nav.models.Cliente
 
 class StatsActivity : AppCompatActivity() {
     var clientes: List<Cliente> = arrayListOf()
 
-    inner class ClientesAdapter : BaseAdapter() {
-        override fun getCount(): Int {
-            return clientes.size
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stats)
+
+        var media : Float = 0.0F
+
+        Backend.getAllClientes { clientes }
+
+        for (cliente in clientes){
+            media += cliente.idade!!
         }
 
-        override fun getItem(position: Int): Any {
-            return clientes[position]
-        }
+        val mediaTextView = findViewById<TextView>(R.id.resultAge)
+        mediaTextView.text = "$media"
 
-        override fun getItemId(position: Int): Long {
-            return 0
-        }
-
-        override fun getView(position: Int, view: View?, viewGroup: ViewGroup?): View {
-
-            val rootView = layoutInflater.inflate(R.layout.row_cavalo,viewGroup,false)
-
-            val result = rootView.findVu
-
-            for (a in clientes) {
-
-                var quantityClients = 0
-                var media = 0
-                if (quantityClients < clientes.size) {
-                    media += clientes[a].idade
-                }
-            }
-
-            return rootView
-        }
-
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_stats)
-
-            val clientesAdapter = ClientesAdapter()
-
-            Backend.getAllClientes {
-                clientes = it
-                clientesAdapter.notifyDataSetChanged()
-            }
-
-
-        }
     }
 }
+
