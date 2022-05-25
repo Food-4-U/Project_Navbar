9a8f3844284861476b7d1d9d1792207feaa0ad70
XIceQueenX <gloriapink.k@gmail.com>
2022-05-25 09:43:36 +0100

    add toJSON functions

--- app/src/main/java/com/grupo1/food4u_nav/models/Item_Menu.kt
+++ app/src/main/java/com/grupo1/food4u_nav/models/Item_Menu.kt
@@ -1,5 +1,6 @@
 package com.grupo1.food4u_nav.models
 
+import org.json.JSONObject
 import java.sql.Blob
 
 class Item_Menu {
@@ -40,4 +41,38 @@ class Item_Menu {
     var fav : Boolean
 
 
+    fun toJSON() : JSONObject {
+        val jsonObject = JSONObject()
+        jsonObject.put("id_item", id_item)
+        jsonObject.put("nome", nome)
+        jsonObject.put("preco", preco)
+        jsonObject.put("temp_prep", temp_prep)
+        jsonObject.put("avaliacao", avaliacao)
+        jsonObject.put("url", url)
+        jsonObject.put("destaque", destaque)
+        jsonObject.put("id_categoria", id_categoria)
+        jsonObject.put("id_subcategoria", id_subcategoria)
+        jsonObject.put("fav", fav)
+        return jsonObject
+    }
+
+    //
+    companion object{
+        fun fromJSON(jsonObject: JSONObject): Item_Menu{
+            return Item_Menu(
+                jsonObject["id_item"] as Int,
+                jsonObject["nome"] as String,
+                jsonObject["preco"] as Float,
+                jsonObject["temp_prep"] as Int,
+                jsonObject["avaliacao"] as Float,
+                jsonObject["url"] as String,
+                jsonObject["destaque"] as Boolean,
+                jsonObject["id_categoria"] as Int,
+                jsonObject["id_subcategoria"] as Int,
+                jsonObject["fav"] as Boolean
+            )
+        }
+    }
+
 }
+
