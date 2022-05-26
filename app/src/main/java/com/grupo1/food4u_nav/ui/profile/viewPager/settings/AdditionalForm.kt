de3e93638f8f5056b1cac771ed1e30a7e64c9070
DimoN <diogo.vsimoes@gmail.com>
2022-05-26 11:09:59 +0100

    Added back button!

--- app/src/main/java/com/grupo1/food4u_nav/ui/profile/viewPager/settings/AdditionalForm.kt
+++ app/src/main/java/com/grupo1/food4u_nav/ui/profile/viewPager/settings/AdditionalForm.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -16,21 +18,25 @@ import com.grupo1.food4u_nav.databinding.FragmentAdditionFormBinding
 class AdditionalForm : Fragment() {
     private lateinit var _binding: FragmentAdditionFormBinding
 
+
+
+
     private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         _binding = FragmentAdditionFormBinding.inflate(inflater, container, false)
 
         val root: View = binding.root
 
         val backBtn: Button = root.findViewById(R.id.additionForm_backBtn)
         backBtn.setOnClickListener {
-            Toast.makeText(activity,"oi",Toast.LENGTH_LONG).show()
+            Toast.makeText(activity,"Já foste ohhhh mano!",Toast.LENGTH_LONG).show()
         }
-
 
         return root
     }
+
 }
