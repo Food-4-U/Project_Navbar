8fd57fe4dc8afccb2c14b25ff903fdf70915aa01
XIceQueenX <gloriapink.k@gmail.com>
2022-05-28 18:54:31 +0100

    add clickButton in rv_hottest

--- app/src/main/java/com/grupo1/food4u_nav/adapters/HottestAdapter.kt
+++ app/src/main/java/com/grupo1/food4u_nav/adapters/HottestAdapter.kt
@@ -1,17 +1,22 @@
 package projeto.ipca.food4u.grupoI.adapters
 
-import android.icu.number.NumberFormatter.with
-import android.icu.number.NumberRangeFormatter.with
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RatingBar
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.internal.ContextUtils.getActivity
+import com.grupo1.food4u_nav.ProductDetailsActivity
 import com.grupo1.food4u_nav.R
 import com.grupo1.food4u_nav.models.Item_Menu
+import com.grupo1.food4u_nav.ui.home.HomeFragment
 import com.squareup.picasso.Picasso
+import java.security.AccessController.getContext
 
 
 class HottestAdapter(val itens: List<Item_Menu>) : RecyclerView.Adapter<HottestAdapter.ViewHolder>() {
@@ -37,6 +42,10 @@ class HottestAdapter(val itens: List<Item_Menu>) : RecyclerView.Adapter<HottestA
 
         var imageURL = itens[position].url
         Picasso.get().load(imageURL).resize(800,650).into(holder.photoFood)
+
+        holder.itemView.setOnClickListener {
+            //Vai para outra página
+        }
     }
 
     override fun getItemCount(): Int {
