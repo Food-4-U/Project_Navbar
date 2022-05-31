975d70b7d1907e756b0f3abb2f77a4884cef3d5e
“AutoMendesgit config --global user.email “automendes30@gmail.com <automendes30@gmail.com>
2022-05-31 15:05:44 +0100

    - Try to show itens in menu fragment

--- app/src/main/java/com/grupo1/food4u_nav/adapters/MenuAdapter.kt
+++ app/src/main/java/com/grupo1/food4u_nav/adapters/MenuAdapter.kt
@@ -1,17 +1,26 @@
 package com.grupo1.food4u_nav.adapters
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.grupo1.food4u_nav.ProductDetailsActivity
 import com.grupo1.food4u_nav.R
 import com.grupo1.food4u_nav.models.CategoryType
+import com.grupo1.food4u_nav.models.Item_Menu
+import com.squareup.picasso.Picasso
 
-class MenuAdapter (val itens: List<CategoryType>) : RecyclerView.Adapter<MenuAdapter.ViewHolder>() {
+class MenuAdapter(val context: Context, val itens: List<Item_Menu>) : RecyclerView.Adapter<MenuAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var categoryName = itemView.findViewById<TextView>(R.id.categoryText)
+        var photoFood = itemView.findViewById<ImageView>(R.id.photoFood)
+        var productName = itemView.findViewById<TextView>(R.id.productTitle)
+        var priceProduct = itemView.findViewById<TextView>(R.id.priceProduct)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -19,8 +28,19 @@ class MenuAdapter (val itens: List<CategoryType>) : RecyclerView.Adapter<MenuAda
         return ViewHolder(view)
     }
 
+    @SuppressLint("RestrictedApi")
     override fun onBindViewHolder(holder: MenuAdapter.ViewHolder, position: Int) {
-        holder.categoryName.text = itens[position].name
+        holder.productName.text = itens[position].nome
+        holder.priceProduct.text = itens[position].preco.toString().plus("€")
+
+        var imageURL = itens[position].url
+        Picasso.get().load(imageURL).resize(800,650).into(holder.photoFood)
+
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context, ProductDetailsActivity::class.java)
+            intent.putExtra("id_item", itens[position].id_item)
+            context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int {
