0d4d2aab28077ddc213ebee6e0a3d29ddace8691
DimoN <diogo.vsimoes@gmail.com>
2022-06-02 11:34:30 +0100

    Spinners

--- app/src/main/java/com/grupo1/food4u_nav/ui/admin/NewItemFragment.kt
+++ app/src/main/java/com/grupo1/food4u_nav/ui/admin/NewItemFragment.kt
@@ -6,18 +6,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Switch
-import android.widget.Toast
+import android.widget.*
 import com.google.android.material.button.MaterialButton
 import com.grupo1.food4u_nav.R
+import com.grupo1.food4u_nav.databinding.FragmentAdditionFormBinding
 import com.grupo1.food4u_nav.databinding.FragmentDeskBinding
+import com.grupo1.food4u_nav.databinding.FragmentNewItemBinding
 import com.grupo1.food4u_nav.models.Item_Menu
 
 class NewItemFragment : Fragment() {
 
-    private var _binding: FragmentDeskBinding? = null
+    private var _binding: FragmentNewItemBinding? = null
     private val binding get() = _binding!!
 
 
@@ -28,24 +27,39 @@ class NewItemFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+        _binding = FragmentNewItemBinding.inflate(inflater, container, false)
         val view = inflater.inflate(R.layout.fragment_new_item, container, false)
 
         val add_foodName = view.findViewById<EditText>(R.id.add_foodName)
         val add_foodPrice = view.findViewById<EditText>(R.id.add_foodPrice)
         val add_foodTime = view.findViewById<EditText>(R.id.add_foodTime)
         val add_foodURL = view.findViewById<EditText>(R.id.add_foodUrl)
-        val add_foodCategory = view.findViewById<EditText>(R.id.add_foodIdCategory)
-        val add_foodSubCategory = view.findViewById<EditText>(R.id.add_foodIdSubCategory)
         val add_newItem = view.findViewById<MaterialButton>(R.id.add_newItem)
         val highlight = view.findViewById<Switch>(R.id.switch_highlight)
+
+
+        val categoriaSpinner = view.findViewById<Spinner>(R.id.textInputLayout9)
+        val Categoria = arrayOf("Marshmellow", "Cona", "Peixe", "Chourica", "Mamocas")
+        categoriaSpinner?.adapter = ArrayAdapter(
+            activity?.applicationContext!!,
+            R.layout.dropdownitem, Categoria
+        )
+
+        val subCategoriaSpinner = view.findViewById<Spinner>(R.id.textInputLayout10)
+        val SubCategoria = arrayOf("XXX", "XXX", "XXX", "XXX", "XXX")
+        subCategoriaSpinner?.adapter = ArrayAdapter(
+            activity?.applicationContext!!,
+            R.layout.dropdownitem, SubCategoria
+        )
+
 
         add_newItem.setOnClickListener {
             var item = Item_Menu(
                 null, add_foodName.text.toString(),
                 add_foodPrice.text.toString().toDouble(),
                 add_foodTime.text.toString().toInt(),
-                highlight.isChecked, add_foodURL.text.toString(), add_foodCategory.text.toString().toInt(),
-                add_foodSubCategory.text.toString().toInt(), 0.0
+                highlight.isChecked, add_foodURL.text.toString(), null,
+                null, 0.0
             )
 
             Backend.addItem(item) {
@@ -56,6 +70,8 @@ class NewItemFragment : Fragment() {
                     Toast.makeText(requireActivity(), "ERRO", Toast.LENGTH_SHORT).show()
                 }
             }
+
+
 
             /*  addItem(item) {
                   if (it)
