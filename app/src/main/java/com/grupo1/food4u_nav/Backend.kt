b96eae518f8c2389ba2381ee09ed9a195e86d973
“AutoMendesgit config --global user.email “automendes30@gmail.com <automendes30@gmail.com>
2022-06-05 09:24:41 +0100

    - Added update item in Backend.kt

--- app/src/main/java/com/grupo1/food4u_nav/Backend.kt
+++ app/src/main/java/com/grupo1/food4u_nav/Backend.kt
@@ -318,6 +318,29 @@ object Backend {
         }
     }
 
+    fun updateItem(id: Int, item: Item_Menu, callback : ((Boolean)->Unit) ) {
+
+        GlobalScope.launch (Dispatchers.IO) {
+            val mediaType = "application/json; charset=utf-8".toMediaType()
+            val body: RequestBody = RequestBody.create(
+                    mediaType, item.toJSON().toString())
+            val client = OkHttpClient()
+            val request = Request.Builder()
+                .url(BASE_API + "Item/Update/" + "/" + id)
+                .put(body)
+                .build()
+            client.newCall(request).execute().use { response ->
+                var result = response.body!!.string()
+                var resultJSONObject = JSONObject(result)
+
+                GlobalScope.launch (Dispatchers.Main){
+                    val status = resultJSONObject.getString("status")
+                    callback.invoke(status == "ok")
+                }
+            }
+        }
+    }
+
     // CATEGORIAS E SUBCATEGORIAS NOME
 
     fun getNameCategory(id: Int, callback: ((CategoryType) -> Unit)) {
