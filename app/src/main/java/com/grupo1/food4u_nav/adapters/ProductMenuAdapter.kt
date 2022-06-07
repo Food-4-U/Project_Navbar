8893cba0b494468372d7e9e32771489ee9f134b4
XIceQueenX <gloriapink.k@gmail.com>
2022-06-07 23:03:35 +0100

    add favorites model

--- app/src/main/java/com/grupo1/food4u_nav/adapters/ProductMenuAdapter.kt
+++ app/src/main/java/com/grupo1/food4u_nav/adapters/ProductMenuAdapter.kt
@@ -17,18 +17,20 @@ import com.google.android.material.imageview.ShapeableImageView
 import com.google.android.material.internal.ContextUtils.getActivity
 import com.grupo1.food4u_nav.ProductDetailsActivity
 import com.grupo1.food4u_nav.R
+import com.grupo1.food4u_nav.models.Favorites
 import com.grupo1.food4u_nav.models.Item_Menu
 import com.grupo1.food4u_nav.models.SubCategories
 import com.squareup.picasso.Picasso
 
 
-class ProductMenuAdapter(val context: Context, val itens: List<Item_Menu>) : RecyclerView.Adapter<ProductMenuAdapter.ViewHolder>() {
+class ProductMenuAdapter(val context: Context, val itens: List<Item_Menu>/*, val fav : List<Favorites>*/) : RecyclerView.Adapter<ProductMenuAdapter.ViewHolder>() {
 
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var photoFood = itemView.findViewById<ImageView>(R.id.productPhoto)
         var nameFood = itemView.findViewById<TextView>(R.id.productTitle)
         var foodprice = itemView.findViewById<TextView>(R.id.priceProduct)
+        var likebtnMenu = itemView.findViewById<ImageView>(R.id.likebtnMenu)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -50,10 +52,33 @@ class ProductMenuAdapter(val context: Context, val itens: List<Item_Menu>) : Rec
             intent.putExtra("id_item", itens[position].id_item)
             context.startActivity(intent)
         }
+
+       /* holder.isChecked = itens[position].
+
+        changeIconFav(position,holder)
+
+        holder.likebtnMenu.setOnClickListener {
+            changeIconFav(position,holder)
+        }*/
     }
 
 
     override fun getItemCount(): Int {
         return itens.size
     }
+
+
+    /*fun changeIconFav(position: Int,holder: ProductMenuAdapter.ViewHolder){
+        for (i in 0 until fav.size){
+            if (itens[position].id_item == fav[i].itens_id_item && )
+        }
+        if (itens[position].id_item == fav){
+            holder.likeBtn.setImageResource(R.drawable.ic_heartfill)
+            fav[position].fav = false
+        }
+        else{
+            holder.likeBtn.setImageResource(R.drawable.ic_heartnotfill)
+            fav[position].fav = true
+        }
+    }*/
 }
