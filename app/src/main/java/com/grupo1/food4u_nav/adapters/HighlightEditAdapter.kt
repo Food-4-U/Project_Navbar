f3889ef040028e08b925f44edf7225c632c92702
XIceQueenX <gloriapink.k@gmail.com>
2022-06-09 09:50:42 +0100

    add searchBar

--- app/src/main/java/com/grupo1/food4u_nav/adapters/HighlightEditAdapter.kt
+++ app/src/main/java/com/grupo1/food4u_nav/adapters/HighlightEditAdapter.kt
@@ -50,6 +50,7 @@ class HighlightEditAdapter (val context: Context, val itens: List<Item_Menu>) : 
                      Toast.makeText(context,"Nao removeu dos highLights",Toast.LENGTH_SHORT)
 
              }
+            notifyDataSetChanged()
         }
     }
 
