f222fa5bc8c5d4822a0dfbe8f5b3b8750aeaa4d3
“AutoMendesgit config --global user.email “automendes30@gmail.com <automendes30@gmail.com>
2022-06-15 19:43:06 +0100

    - Fatura starting

--- app/src/main/java/com/grupo1/food4u_nav/adapters/OrderDetailAdapter.kt
+++ app/src/main/java/com/grupo1/food4u_nav/adapters/OrderDetailAdapter.kt
@@ -11,22 +11,25 @@ import com.grupo1.food4u_nav.models.ItensPedido
 class OrderDetailAdapter(val pedido: ItensPedido) : RecyclerView.Adapter<OrderDetailAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var cardNumber = itemView.findViewById<TextView>(R.id.textView63)
+        var productName = itemView.findViewById<TextView>(R.id.produtoNameFaturaROW)
+        var productQtd = itemView.findViewById<TextView>(R.id.quantidadeFaturaROW)
+        // preço unit
+        var productTotal = itemView.findViewById<TextView>(R.id.precoROW)
 
     }
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        var view = LayoutInflater.from(parent.context).inflate(R.layout.row_orderitens, parent, false)
+        var view = LayoutInflater.from(parent.context).inflate(R.layout.row_fatura, parent, false)
         return ViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        return pedido.size
-    }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.cardNumber.text = pedido[position].
+        //holder.productName =
+        //holder.productQtd =
+        //preço unit
+        //holder productTotal =
 
 
     }
